855b6b139e083b624c0e810650129c77bdbb5e2b
Your Name <you@example.com>
2024-02-07 16:56:38 +0100

    add getTimeDifferenceInMinutes to timeHelpers

--- src/helpers/timeHelpers.ts
+++ src/helpers/timeHelpers.ts
@@ -1,5 +1,14 @@
 import { parse } from 'date-fns'
 
+export function getTimeDifferenceInMinutes(from: Date, to: Date) {
+    const timeFrom = from.getTime()
+    const timeTo = to.getTime()
+
+    const timeDifference = (timeTo - timeFrom) / (1000 * 60)
+
+    return Math.abs(timeDifference)
+}
+
 export function convertToDate(date: string, refDate: Date): Date {
     return parse(date, 'HH:mm', refDate)
 }
