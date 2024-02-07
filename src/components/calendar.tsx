3be5b3611069793d3900781360ccd7dc50e1efda
Your Name <you@example.com>
2024-02-07 16:33:03 +0100

    create calendar basic structure

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,3 +1,5 @@
+import { EventList } from '../types'
+
 interface ICalendarProps {
     calendarRange: {
         calendarStart: Date
@@ -5,3 +7,16 @@ interface ICalendarProps {
     }
     eventList: EventList
 }
+
+function Calendar({ eventList, calendarRange: { calendarEnd, calendarStart } }: ICalendarProps) {
+    console.log('eventList: ', eventList)
+    console.log('calendarEnd: ', calendarEnd)
+    console.log('calendarStart: ', calendarStart)
+    return (
+        <div className="Calendar">
+            <span>Calendar</span>
+        </div>
+    )
+}
+
+export default Calendar
