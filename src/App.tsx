265abc66bcfa006d96c2f6eaf86b3613fb964044
Your Name <you@example.com>
2024-02-07 16:24:44 +0100

    add useEventList hook to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import { useEventList } from './hooks/useEventList'
+
 function App() {
+    const eventList = useEventList()
+    console.log('eventList enriched: ', eventList)
+
     return (
         <div>
             <span>Hello World</span>
