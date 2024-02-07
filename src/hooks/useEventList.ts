71574ac0580844288c3088a0ec8736e8197110aa
Your Name <you@example.com>
2024-02-07 16:24:12 +0100

    use computeOverlap in useEventList

--- src/hooks/useEventList.ts
+++ src/hooks/useEventList.ts
@@ -1,12 +1,11 @@
 import { CALENDAR_RELATIVE_REF_DATE } from '../config'
 import rawEventList from '../eventList.json'
-import { addTimeRangeToRawEventList } from '../helpers/eventEnricherHelpers'
+import { addTimeRangeToRawEventList, computeOverlap } from '../helpers/eventEnricherHelpers'
 import { IRawEvent, EventList } from '../types'
 
 function enrichRawEventList(eventList: IRawEvent[]): EventList {
     const builtEventList = addTimeRangeToRawEventList(eventList, CALENDAR_RELATIVE_REF_DATE)
-    console.log('builtEventList:', builtEventList)
-    return []
+    return computeOverlap(builtEventList)
 }
 
 export function useEventList() {
